3c381c4798f5553ab606835cf0445fd659e6501b
LuSkywalker <610721230@gms.ndhu.edu.tw>
2020-08-19 09:28:56 +0800

    Create main.h

---
+++ cpp/main.h
@@ -0,0 +1,18 @@
+/**
+ * @file        main.h
+ * @author      LuSkywalker (info@lusw.dev)
+ * @brief       Header file of main.c
+ * @version     1.0
+ * @date        2020-08-18
+ * 
+ * @copyright   Copyright (c) 2020 LuSkywalker
+ * 
+ */
+
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <iomanip>
+#include "hex2signed.h"
+
+#endif /* __MAIN_H__ */
