33d2806d97a2175974f73c95ddd57f0445e806bb
LuSkywalker <610721230@gms.ndhu.edu.tw>
2020-08-19 13:51:57 +0800

    Update main.h

--- cpp/main.h
+++ cpp/main.h
@@ -1,5 +1,5 @@
 /**
- * @file        main.h
+ * @file        cpp/main.h
  * @author      LuSkywalker (info@lusw.dev)
  * @brief       Header file of main.c
  * @version     1.0
@@ -9,10 +9,10 @@
  * 
  */
 
-#ifndef __MAIN_H__
-#define __MAIN_H__
+#ifndef __CPP_MAIN_H__
+#define __CPP_MAIN_H__
 
 #include <iomanip>
 #include "hex2signed.h"
 
-#endif /* __MAIN_H__ */
+#endif /* __CPP_MAIN_H__ */
